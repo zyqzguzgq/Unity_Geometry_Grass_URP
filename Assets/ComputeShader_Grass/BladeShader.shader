7b7a49e8398c4f7fa2f649be8109daed4c772a23
unknown <BigComputer>
2024-02-29 01:12:38 +0800

    add Blade_UV

--- Assets/ComputeShader_Grass/BladeShader.shader
+++ Assets/ComputeShader_Grass/BladeShader.shader
@@ -19,6 +19,7 @@ Shader "Unlit/BladeShader"
             {
                 float4 col : COLOR0;
                 float4 positionCS : SV_POSITION;
+                float2 uv : TEXCOORD1;
             };
 
             struct BladeData
@@ -30,16 +31,23 @@ Shader "Unlit/BladeShader"
             StructuredBuffer<BladeData> _BladeDataBuffer;
 
             StructuredBuffer<float3> _Positions;
+
+            StructuredBuffer<float2> _UV;
+
             uniform uint _BaseVertexIndex;
             uniform float4x4 _ObjectToWorld;
             uniform float _NumInstances;
+            uniform float _Interval;
 
             v2f vert(uint id : SV_VertexID , uint instanceID : SV_InstanceID)
             {
                 v2f o;
                 float3 pos = _Positions[id + _BaseVertexIndex];
-                float4 wpos = mul(_ObjectToWorld, float4(pos , 1.0f)) + float4(instanceID , 0,0,0);
+                float BladePosX = instanceID % 100 * _Interval;
+                float BladePosZ = instanceID / 100 * _Interval;
+                float4 wpos = mul(_ObjectToWorld, float4(pos , 1.0f)) + float4(BladePosX ,0,BladePosZ,0);
                 o.positionCS = mul(UNITY_MATRIX_VP, wpos);
+                o.uv = _UV[id + _BaseVertexIndex];
                 o.col = float4( 1.f, 0.f, 0.0f, 0.0f);
 
 
@@ -51,7 +59,8 @@ Shader "Unlit/BladeShader"
 
             float4 frag(v2f i) : SV_Target
             {
-                return i.col;
+                float4 finalColor = lerp(float4(0,1,0,1),float4(0,0,1,1),i.uv.y);
+                return finalColor;
             }
             ENDHLSL
         }
--- Assets/ComputeShader_Grass/DrawBlade.cs
+++ Assets/ComputeShader_Grass/DrawBlade.cs
@@ -7,37 +7,56 @@ public class DrawBlade : MonoBehaviour
     public ComputeShader computeShader;
     public Material material;
     public Mesh mesh;
+    public float fieldWidth = 100;
+    public float interval = 0.5f;
 
+    
     GraphicsBuffer meshTriangles;
     GraphicsBuffer meshPositions;
-
+    GraphicsBuffer meshUV;
     ComputeBuffer mBladeDataBuffer;
-    const int mBladeCount = 20000;
     int kernelId;
 
-    struct BladeData {
+    struct BladeData 
+    {
         public Vector3 pos;
         public Color color;
     }
+    
+
+    
 
     void Start() {
         //structÖÐÒ»¹²7¸öfloat£¬size=28
+        int mBladeCount = (int)(fieldWidth * fieldWidth / interval / interval);
+        mBladeCount = 10000;
         mBladeDataBuffer = new ComputeBuffer(mBladeCount, 28);
         BladeData[] BladeDatas = new BladeData[mBladeCount];
         mBladeDataBuffer.SetData(BladeDatas);
+
         kernelId = computeShader.FindKernel("UpdateBlade");
+
+
 
         meshTriangles = new GraphicsBuffer(GraphicsBuffer.Target.Structured, mesh.triangles.Length, sizeof(int));
         meshTriangles.SetData(mesh.triangles);
         meshPositions = new GraphicsBuffer(GraphicsBuffer.Target.Structured, mesh.vertices.Length, 3 * sizeof(float));
         meshPositions.SetData(mesh.vertices);
+        meshUV = new GraphicsBuffer(GraphicsBuffer.Target.Structured , mesh.uv.Length , 2 * sizeof(float));
+        meshUV.SetData(mesh.uv);
     }
 
     void Update() {
+        int mBladeCount = (int)(fieldWidth * fieldWidth / interval / interval);
+        mBladeCount = 10000;
+
         computeShader.SetBuffer(kernelId, "BladeBuffer", mBladeDataBuffer);
         computeShader.SetFloat("Time", Time.time);
         computeShader.Dispatch(kernelId, mBladeCount / 1000, 1, 1);
         material.SetBuffer("_BladeDataBuffer", mBladeDataBuffer);
+
+
+
 
         RenderParams rp = new RenderParams(material);
         rp.worldBounds = new Bounds(Vector3.zero, 10000*Vector3.one); // use tighter bounds
@@ -46,13 +65,13 @@ public class DrawBlade : MonoBehaviour
         rp.matProps.SetInt("_BaseVertexIndex", (int)mesh.GetBaseVertex(0));
         rp.matProps.SetMatrix("_ObjectToWorld", Matrix4x4.TRS(new Vector3(-4.5f, 0, 0), Quaternion.identity, new Vector3(100f, 100f, 100f)));
         rp.matProps.SetFloat("_NumInstances", 10.0f);
-        Graphics.RenderPrimitivesIndexed(rp, MeshTopology.Triangles, meshTriangles, meshTriangles.count, (int)mesh.GetIndexStart(0), 10);
+        rp.matProps.SetFloat("_Interval" , interval);
+        rp.matProps.SetBuffer("_UV" , meshUV);
+
+        Graphics.RenderPrimitivesIndexed(rp, MeshTopology.Triangles, meshTriangles, meshTriangles.count, (int)mesh.GetIndexStart(0), mBladeCount);
     }
 
-    void OnRenderObject() {
-        material.SetPass(0);
-        Graphics.DrawProceduralNow(MeshTopology.Points, mBladeCount);
-    }
+
 
     void OnDestroy() {
         mBladeDataBuffer.Release();
@@ -62,5 +81,7 @@ public class DrawBlade : MonoBehaviour
         meshTriangles = null;
         meshPositions?.Dispose();
         meshPositions = null;
+        meshUV?.Dispose();
+        meshUV = null;
     }
 }
